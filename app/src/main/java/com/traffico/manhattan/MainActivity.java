671d8a237263aaac3b99fcb01d2a3cc024defb6c
Edwin Piragauta <edwin.piragauta@gmail.com>
2018-10-11 09:42:03 -0500

    Access to profile, name, last name and email from facebook

--- app/src/main/java/com/traffico/manhattan/MainActivity.java
+++ app/src/main/java/com/traffico/manhattan/MainActivity.java
@@ -9,9 +9,15 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("onSuccess", "UserId: " + loginResult.getAccessToken().getUserId());
                 Log.i("onSuccess", "Token: " + loginResult.getAccessToken().getToken());
                 Log.i("onSuccess", "Recently Granted Permissions " + loginResult.getRecentlyGrantedPermissions());
+                setFacebookData(loginResult);
             }
 
             @Override
@@ -52,9 +59,53 @@ public class MainActivity extends AppCompatActivity {
                 // App code
                 Log.i("onError", "Error" + exception.getMessage());
             }
+            //
+            //
+            private void setFacebookData(final LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                // Application code
+                                try {
+                                    Log.i("Response", response.toString());
+
+                                    String email = response.getJSONObject().getString("email");
+                                    String firstName = response.getJSONObject().getString("first_name");
+                                    String lastName = response.getJSONObject().getString("last_name");
+                                    //String gender = response.getJSONObject().getString("gender");
+
+
+                                    Profile profile = Profile.getCurrentProfile();
+                                    String id = profile.getId();
+                                    String link = profile.getLinkUri().toString();
+                                    Log.i("Link", link);
+                                    if (Profile.getCurrentProfile() != null) {
+                                        Log.i("Login", "ProfilePic" + Profile.getCurrentProfile().getProfilePictureUri(200, 200));
+                                    }
+
+                                    Log.i("Login" + "Email", email);
+                                    Log.i("Login" + "FirstName", firstName);
+                                    Log.i("Login" + "LastName", lastName);
+                                    //Log.i("Login" + "Gender", gender);
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,email,first_name,last_name");
+                //parameters.putString("fields", "id,email,first_name,last_name,gender");
+                request.setParameters(parameters);
+                request.executeAsync();
+                //
+            }
+            //
         });
         //
-
     }
 
     @Override
@@ -62,4 +113,5 @@ public class MainActivity extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
     }
+
 }
